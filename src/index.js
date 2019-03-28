0797aa5a028199d044f400363340ff92fe186536
Pavol Hejny <me@pavolhejny.com>
2019-03-28 16:31:06 +0100

    Basic talks loading

--- src/index.js
+++ src/index.js
@@ -1,50 +1,85 @@
-var slideshow = remark.create({
-    ratio: '16:9',
-});
+main();
 
-for (const slideContainer of document.querySelectorAll(
-    '.remark-slide-container',
-)) {
-    const slide = slideContainer.querySelector('.remark-slide-content'); //todo rename to slideContent
+async function main() {
+    const id = getParameter('id');
 
-    //-----------------------------------------Background image //todo maybe to render f
-    {
-        const images = slide.querySelectorAll('img');
-        if (images.length === 1) {
-            const image = images[0];
-            image.onload = () => {
-                if (
-                    image.width >
-                    400 /*slide.getBoundingClientRect().width*0.5*/
-                ) {
-                    image.style.display = 'none';
+    try {
+        const content = await fetchTalk(id);
 
-                    slide.style.background = `url(${image.src})`;
-                    slide.style.backgroundSize = 'cover';
-                    slide.style.backgroundPosition = 'center center';
-                    slide.classList.add('remark-slide-content-with-image');
+        console.log('content', content);
 
-                    for (const heading of slide.querySelectorAll('h1')) {
-                        wrapElement(heading);
-                    }
-                }
-            };
-        }
+        document.getElementById('source').innerHTML = content;
+        /*const slideshow = */
+        remark.create({
+            ratio: '16:9',
+        });
+
+        postprocessRemark();
+    } catch (error) {
+        const indexMarkdown = await fetchContentFile('index.md');
+        const indexHtml = markdown.toHTML(indexMarkdown);
+
+        document.getElementById('index').innerHTML = indexHtml;
     }
-    //-----------------------------------------
+}
 
-    //todo also working on forst slude
-    addClassNameListener(
-        slideContainer,
-        () => {
-            if (slideContainer.classList.contains('remark-visible')) {
-                render(slide);
-            } else {
-                unrender(slide);
+function fetchTalk(id) {
+    return fetchContentFile(`${id}.md`);
+}
+
+async function fetchContentFile(contentFile) {
+    const content = await fetch(`./content/${contentFile}`);
+    if (content.status === 404) {
+        throw new Error(`Content file "${contentFile}" not found.`);
+    }
+    return await content.text();
+}
+
+function postprocessRemark() {
+    for (const slideContainer of document.querySelectorAll(
+        '.remark-slide-container',
+    )) {
+        const slide = slideContainer.querySelector('.remark-slide-content'); //todo rename to slideContent
+
+        //-----------------------------------------Background image //todo maybe to render f
+        {
+            const images = slide.querySelectorAll('img');
+            if (images.length === 1) {
+                const image = images[0];
+                image.onload = () => {
+                    if (
+                        image.width >
+                        400 /*slide.getBoundingClientRect().width*0.5*/
+                    ) {
+                        image.style.display = 'none';
+
+                        slide.style.background = `url(${image.src})`;
+                        slide.style.backgroundSize = 'cover';
+                        slide.style.backgroundPosition = 'center center';
+                        slide.classList.add('remark-slide-content-with-image');
+
+                        for (const heading of slide.querySelectorAll('h1')) {
+                            wrapElement(heading);
+                        }
+                    }
+                };
             }
-        },
-        false,
-    );
+        }
+        //-----------------------------------------
+
+        //todo also working on forst slude
+        addClassNameListener(
+            slideContainer,
+            () => {
+                if (slideContainer.classList.contains('remark-visible')) {
+                    render(slide);
+                } else {
+                    unrender(slide);
+                }
+            },
+            false,
+        );
+    }
 }
 
 function render(slide) {
@@ -73,10 +108,10 @@ function renderCodesample(slide) {
         );
         console.log(`Slide is a codesample from "${jsfiddleUriPart}".`);
         slide.innerHTML = `
-        <h1>🚀</h1>
-        <iframe class="left" src="https://jsfiddle.net/${jsfiddleUriPart}/embedded/${type}/dark/"></iframe>
-        <iframe class="right hide-topbar" src="https://jsfiddle.net/${jsfiddleUriPart}/embedded/result/dark/"></iframe>
-      `;
+    <h1>🚀</h1>
+    <iframe class="left" src="https://jsfiddle.net/${jsfiddleUriPart}/embedded/${type}/dark/"></iframe>
+    <iframe class="right hide-topbar" src="https://jsfiddle.net/${jsfiddleUriPart}/embedded/result/dark/"></iframe>
+  `;
         slide.classList.add('remark-slide-content-with-codesample');
     } else {
         console.log(`[x] Slide is not a codesample.`, CODESAMPLE_REGEX);
@@ -89,9 +124,9 @@ function renderSampleBackground(slide) {
         const [full, url] = slide.innerHTML.match(SAMPLE_BACKGROUND_REGEX);
         console.log(`Slide is a sample in background from "${url}".`);
         slide.innerHTML = `
-        <h1>🚀</h1>
-        <iframe src="https://${url}"></iframe>
-      `;
+    <h1>🚀</h1>
+    <iframe src="https://${url}"></iframe>
+  `;
         slide.classList.add('remark-slide-content-with-sample-background');
     } else {
         console.log(
@@ -116,4 +151,17 @@ function addClassNameListener(elem, callback) {
         requestAnimationFrame(check);
     };
     check();
+}
+
+function getParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split('&')
+        .forEach(function(item) {
+            tmp = item.split('=');
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
 }
