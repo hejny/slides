c272192c750179cc35374a8a4614e614b05e40ae
Pavol Hejny <me@pavolhejny.com>
2019-04-04 01:28:52 +0200

    Show errors

--- content/index.md
+++ content/index.md
@@ -3,8 +3,11 @@
 # Talks
 
 - [(2019-04-04, Pilsen) Asynchronní Javascript ](?id=2019/001-asynchronous-javascript/asynchronous-javascript)
-- [(2019-04-04, Pilsen) RxJS ]()
-- [(2019-04-06, Prague) Tvořte úspěšný, ne dokonalý produkt. ]()
-- [(2019-04-11, Brno) Useful browser APIs ]()
-- [(2019-04-13, Pilsen) Tvořte úspěšný, ne dokonalý produkt. ]()
-- [(2019-04-11, Prague) Jsi líný? Uč se programovací jazyky! ]()
+
+# Preparing
+
+- [(2019-04-04, Pilsen) RxJS ](?id=2019/002-rxjs/rxjs)
+- [(2019-04-06, Prague) Tvořte úspěšný, ne dokonalý produkt. ](?id=2019/003-tvorte-uspesny-ne-dokonaly-produkt/tvorte-uspesny-ne-dokonaly-produkt)
+- [(2019-04-11, Brno) Useful browser APIs ](?id=2019/004-useful-browser-apis/useful-browser-apis)
+- [(2019-04-13, Pilsen) Tvořte úspěšný, ne dokonalý produkt. ](?id=2019/003-tvorte-uspesny-ne-dokonaly-produkt/tvorte-uspesny-ne-dokonaly-produkt)
+- [(2019-04-11, Prague) Jsi líný? Uč se programovací jazyky! ](?id=2019/006-jsi-liny-uc-se-programovaci-jazyky/jsi-liny-uc-se-programovaci-jazyky)
--- src/index.js
+++ src/index.js
@@ -3,37 +3,47 @@ import { preprocessRemark } from './preprocessRemark.js';
 
 export async function main() {
     const id = getParameter('id');
+    let errorMessage;
 
-    try {
-        //todo better file name
-        const content = await preprocessRemark(
-            await fetchTalk(id),
-            `./content/${id
-                .split('/')
-                .splice(0, 2)
-                .join('/')}`,
-        );
+    if (id) {
+        try {
+            //todo better file name
+            const content = await preprocessRemark(
+                await fetchTalk(id),
+                `./content/${id
+                    .split('/')
+                    .splice(0, 2)
+                    .join('/')}`,
+            );
 
-        document.getElementById('source').innerHTML = content;
-        /*const slideshow = */
-        remark.create({
-            ratio: '16:9',
-        });
+            document.getElementById('source').innerHTML = content;
+            /*const slideshow = */
+            remark.create({
+                ratio: '16:9',
+            });
 
-        postprocessRemark();
+            postprocessRemark();
 
-        document.title = document.querySelector(
-            '.remark-slide-container h1',
-        ).innerText;
-    } catch (error) {
-        console.error(error);
+            document.title = document.querySelector(
+                '.remark-slide-container h1',
+            ).innerText;
 
-        const indexMarkdown = await fetchContentFile('index.md');
-        const indexHtml = markdown.toHTML(indexMarkdown);
-
-        document.getElementById('index').innerHTML = indexHtml;
-        document.getElementById('source').style.display = 'none';
+            return;
+        } catch (error) {
+            console.error(error);
+            errorMessage = error.message;
+        }
     }
+
+    let indexMarkdown = await fetchContentFile('index.md');
+
+    if (errorMessage) {
+        indexMarkdown = `_${errorMessage}_\n\n${indexMarkdown}`;
+    }
+    const indexHtml = markdown.toHTML(indexMarkdown);
+
+    document.getElementById('index').innerHTML = indexHtml;
+    document.getElementById('source').style.display = 'none';
 }
 
 function fetchTalk(id) {
